54dea256abc3850dd4f12d65522d9bf8ea203900
rifat-54 <rifat999por@gmail.com>
2024-12-04 11:49:14 +0600

    added coffee sever

--- index.js
+++ index.js
@@ -10,7 +10,9 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.swu9d.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.umkvz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+console.log(uri);
 
 const client = new MongoClient(uri, {
     serverApi: {
@@ -30,6 +32,8 @@ async function run() {
 
         const database = client.db('coffeeDB');
         const coffeeCollection = database.collection('coffee');
+
+        const userCollection=client.db('coffeeDB').collection('user')
 
 
         app.get('/coffee', async (req, res) => {
@@ -74,6 +78,41 @@ async function run() {
             res.send(result);
         })
 
+        // user realted
+
+        app.get('/users',async(req,res)=>{
+            const cursor=userCollection.find()
+            const result=await cursor.toArray()
+            res.send(result)
+        })
+
+        app.post('/users',async(req,res)=>{
+            const newUser=req.body;
+            console.log('creating new user',newUser);
+            const result=await userCollection.insertOne(newUser)
+            res.send(result)
+        })
+
+        app.patch('/users',async(req,res)=>{
+            const email=req.body.email;
+            const filter={email}
+            const updateUser={
+                $set:{
+                    lastSignInTime:req?.body?.lastSignInTime
+
+
+                }
+            }
+            const result=await userCollection.updateOne(filter,updateUser)
+            res.send(result)
+        })
+
+        app.delete('/users/:id',async(req,res)=>{
+            const id=req.params.id;
+            const query={_id:new ObjectId(id)}
+            const result=await userCollection.deleteOne(query)
+            res.send(result)
+        })
 
 
     } finally {
